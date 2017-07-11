e47224fc6347d2b90ac05deea4e2d91427848302
Pu Du <rocketsboy@gmail.com>
2017-07-11 00:24:20 -0500

    easier way to get voronoi cell

--- order/avc.py
+++ order/avc.py
@@ -12,13 +12,14 @@ from __future__ import division, print_function
 from six.moves import range
 
 import numpy as np
+from scipy.spatial import ConvexHull
 
 class VoronoiCell(object):
     """asphericity of the Voronoi cell"""
     def __init__(self):
         pass
     
-    def compute_vc(self, planes):
+    def compute_vc(self, points):
         """compute the Voronoi cell"""
         #total area of all planes
         S = 0.0
@@ -27,21 +28,8 @@ class VoronoiCell(object):
         V = 0.0
 
         #compute S and V
-        for plane in planes:
-           
-            outter_p = 0.0
-            for i in range(1, len(plane)-1):
-                outter_p += np.linalg.norm(np.outter((plane[i] - plane[0]),
-                                                     (plane[i+1] - plane[0])))
-
-            vol = 0.0
-            for i in range(1, len(plane)-1):
-                vol += np.linalg.norm(np.dot(np.outter(plane[0], plane[i]),
-                                             plane[i+1]))    
-            outter_p *= 0.5
-            vol *= 1 / 6
-            S += outter_p
-            V += vol
+        S = ConvexHull(points).area
+        V = ConvexHull(points).volumes
 
         #voronoi cell
         eta = S ** 3 / (36 * np.pi * V ** 2)
