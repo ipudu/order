48d4b6d052a06105aee7b108da25a199b14d78ec
Pu Du <rocketsboy@gmail.com>
2017-08-18 22:03:01 -0500

    water-water angular distribution function

---
+++ order/adf.py
@@ -0,0 +1,9 @@
+###############################################################################
+# -*- coding: utf-8 -*-
+# Order: A tool to characterize the local structure of liquid water 
+#        by geometric order parameters
+# 
+# Authors: Pu Du
+# 
+# Released under the MIT License
+###############################################################################
