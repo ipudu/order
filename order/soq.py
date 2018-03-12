8d48bfceac393f603af2ddbd63016a5f98d701fc
Pu Du <rocketsboy@gmail.com>
2018-03-12 10:48:36 -0500

    only calculate center atom

--- order/soq.py
+++ order/soq.py
@@ -7,3 +7,19 @@
 # 
 # Released under the MIT License
 ###############################################################################
+
+
+from __future__ import print_function, division
+import os
+import six
+from six.moves import range
+
+import numpy as np
+from progress.bar import ChargingBar
+from .util import pbc
+from . import oto
+
+
+class StructureFactor():
+    """Structure Factor"""
+    pass
