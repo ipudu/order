a06f758360987faadb2c2308921409010ff9fdba
Pu Du <rocketsboy@gmail.com>
2018-06-20 14:52:58 -0500

    need work

---
+++ order/interface.py
@@ -0,0 +1,22 @@
+###############################################################################
+# -*- coding: utf-8 -*-
+# Order: A tool to characterize the local structure of liquid water 
+#        by geometric order parameters
+# 
+# Authors: Pu Du
+# 
+# Released under the MIT License
+###############################################################################
+
+from __future__ import division, print_function
+from six.moves import range
+
+import numpy as np
+import yaml
+
+
+class WillardChandler(object):
+    """
+    Identifies the dividing surface using the Willard-Chandler method
+    """
+    pass
